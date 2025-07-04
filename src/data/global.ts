c27647d284cc50ef8505289c05e7b54242123a09
Md Sahil <md61421@gmail.com>
2025-07-04 23:39:02 +0530

    fixes: sitemap.xml urls

--- src/data/global.ts
+++ src/data/global.ts
@@ -84,7 +84,7 @@ export const siteInfo = {
   title: "StreamDB",
   description:
     "StreamDB - A progressive web app (PWA) to preview any movie or tv show with reviews, ratings, description and posters. Acting as a TMDB client, StreamDB gives you access to login into your TMDB account and add movies or tv shows to your watchlist, set as favorites, rate and get personalized recommendations.",
-  url: "https://streamdb.vercel.app",
+  url: "https://streamdb-omega.vercel.app",
   image: "https://i.imgur.com/zmq8tVR.jpeg"
 } as const;
 
