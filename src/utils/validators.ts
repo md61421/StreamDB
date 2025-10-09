921f2b4def784751870343542187167adfcf2a92
Md Sahil <md61421@gmail.com>
2025-10-09 22:23:13 +0530

    feat: add password strength validator

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,3 +1,7 @@
 export const isValidEmail = (email: string): boolean => {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 };
+
+export const isStrongPassword = (password: string): boolean => {
+  return password.length >= 8;
+};
