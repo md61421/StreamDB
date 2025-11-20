0b6cbdb8dd66a01ff8bfb087232e0d8dff678a40
Md Sahil <md61421@gmail.com>
2025-11-20 13:30:30 +0530

    feat: add timeAgo utility

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,3 +1,7 @@
 export const formatDate = (date: Date): string => {
   return new Intl.DateTimeFormat('en-US').format(date);
 };
+
+export const timeAgo = (date: Date): string => {
+  return 'some time ago'; // temp implementation
+};
