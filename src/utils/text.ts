63f938674c88ced10d5f18e37f4802c82a5ec0b9
Md Sahil <md61421@gmail.com>
2025-12-30 16:48:55 +0530

    feat: add capitalizeFirstLetter function

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,3 +1,7 @@
 export const truncate = (str: string, length: number) => {
   return str.length > length ? str.substring(0, length) + '...' : str;
 };
+
+export const capitalizeFirstLetter = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
